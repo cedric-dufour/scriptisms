441f10fbe4b0d29ace2e18c2687c2cba8862227c
Cédric Dufour <cedric.dufour@ced-network.net>
2024-03-31 20:59:33 +0200

    [template.bash] Fix and __CONFIRM

--- template.bash
+++ template.bash
@@ -1,19 +1,21 @@
 #!/bin/bash
-# -*- mode:bash; tab-width:2; sh-basic-offset:2; intent-tabs-mode:nil; -*- ex: set tabstop=2 expandtab: 
+# -*- mode:bash; tab-width:2; sh-basic-offset:2; intent-tabs-mode:nil; -*- ex: set tabstop=2 expandtab:
 # REF: https://github.com/cedric-dufour/scriptisms/blob/master/template.bash
 set -e -o pipefail
 SCRIPT="${0##*/}"
 # shellcheck disable=SC2034
 SCRIPT_DIR="$(dirname "$(realpath -e "${0}")")"
 VERSION='0.0.19730909a'
-trap 'echo "ERROR[${SCRIPT}${ARG_ACTION:+:${ARG_ACTION}]: Internal error" >&2' ERR
+trap 'echo "ERROR[${SCRIPT}${ARG_ACTION:+:${ARG_ACTION}}]: Internal error" >&2' ERR
 
 
 ## Defaults
 : "${SOME_PATH:=/some/path}"
 
 
-## Usage
+## Helpers
+
+# Usage
 __USAGE() {
   cat >&2 << EOF
 USAGE: ${SCRIPT} [<options>] {sample|action|...}
@@ -39,24 +41,47 @@ EOF
 }
 [ $# -lt 1 ] && __USAGE && exit 1
 
-
-## Logging
+# Input/Output
 __INFO() {
   [ -n "${OPT_VERBOSE}" ] && echo "INFO[${SCRIPT}${ARG_ACTION:+:${ARG_ACTION}}]: ${1}"
   return 0
 }
+
 __NOTICE() {
   [ -z "${OPT_SILENT}" ] && echo "NOTICE[${SCRIPT}${ARG_ACTION:+:${ARG_ACTION}}]: ${1}"
   return 0
 }
+
 __WARNING() {
   echo "WARNING[${SCRIPT}${ARG_ACTION:+:${ARG_ACTION}}]: ${1}" >&2
   return 0
 }
+
 __ERROR() {
   echo "ERROR[${SCRIPT}${ARG_ACTION:+:${ARG_ACTION}}]: ${1}" >&2
   [ -n "${2}" ] && exit "${2}"
   return 0
+}
+
+__OPTION() {
+  [ -z "${2}" ] && __ERROR "Missing option argument (${1})" 1
+  return 0
+}
+
+ __CONFIRM() {
+  prompt="${1}"
+  options="${2}"
+  [ -z "${options}" ] && options='N/y'
+  IFS='/' read -r -a a_options <<< "${options}"
+  while true; do
+    read -r -p "CONFIRM[${SCRIPT}]: ${prompt} [${options}] ? " confirm
+    if [ -z "${confirm}" ]; then
+      confirm="${a_options[0]}"
+    fi
+    for option in "${a_options[@]}"; do
+      [ "${confirm,,}" = "${option,,}" ] && echo "${option,,}" && return 0
+    done
+  done
 }
 
 
@@ -65,10 +90,6 @@ OPT_OPTION=
 OPT_VERBOSE="${VERBOSE}"
 OPT_SILENT=
 ARG_ACTION=
-__OPTION() {
-  [ -z "${2}" ] && __ERROR "Missing option argument (${1})" 1
-  return 0
-}
 while [ -n "${1}" ]; do
   case "${1}" in
     '-h'|'--help')
